5bf59c2d3ab39fbc5fe2ef2f0ebe2a58d6114a9d
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2026-01-01 22:11:03 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/sections/Header"
+import Proyects from "./components/sections/Proyects"
+import About from "./components/sections/About"
+import Experience from "./components/sections/Experience"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Proyects />
+      <About />
+      <Experience />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/sections/Proyects.jsx
@@ -0,0 +1,7 @@
+function Proyects() {
+  return (
+    <div>Proyects</div>
+  )
+}
+
+export default Proyects
