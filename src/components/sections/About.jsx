1b9d735cf8a2b706ea665bd8d8d3c7a5ca778cb3
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2026-04-11 00:00:57 -0600

    se creo la seccion de proyectos

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,9 +2,9 @@ import ImagenMiniKurumi from '../../assets/images/mini_kurumi.png'
 import styles from '../../styles/styles.module.css'
 function About() {
   return (
-    <section className='contenedor'>
+    <section className='contenedor pb-[100px]'>
       <img src={ImagenMiniKurumi} className='size-[115px] mx-auto' alt='imagen kurumi' />
-      <div className='flex flex-col gap-6 items-center mt-6'>
+      <div className='flex flex-col gap-6 items-center mt-6 font-tw-cen-mt'>
         <a className={`text-[32px] ${styles.slaches}`}><strong>/</strong><span className='text-black uppercase'>github</span></a>
         <a className={`text-[32px] ${styles.slaches}`}><strong>/</strong><span className='text-black uppercase'>acerca</span></a>
         <a className={`text-[32px] ${styles.slaches}`}><strong>/</strong><span className='text-black uppercase'>contacto</span></a>
--- src/components/sections/Proyects.jsx
+++ src/components/sections/Proyects.jsx
@@ -1,6 +1,45 @@
+import ImagenKurumi from '../../assets/images/kurumi_01.png'
+import styles from '../../styles/styles.module.css'
 function Proyects() {
+
+  const proyectos = [
+    {
+      id: 1,
+      nombre: "Directorio Virtual Doctor Mora",
+      url: "https://directorio-virtual-doctor-mora.vercel.app/",
+    },
+    {
+      id: 2,
+      nombre: "Doctor Mora Travel",
+      url: "https://doctor-mora-travel.vercel.app/",
+    },
+    {
+      id: 3,
+      nombre: "Invitaciones Digitales",
+      url: "https://invitaciones-digitales.vercel.app/",
+    }
+  ]
+
   return (
-    <div>Proyects</div>
+    <section className='bg-rosa-primario w-full h-auto'>
+      <div className="contenedor min-h-[270px] w-full flex">
+        <div className='relative w-1/2 min-h-[270px] flex shrink-0'>
+          <img src={ImagenKurumi} className={`${styles.kurumiProyectos}`} alt='imagen kurumi' />
+        </div>
+        <div className='w-1/2'>
+          <div className='relative -left-[35px] pt-[30px] min-w-[176px]'>
+            <h2 className='text-[32px] font-tw-cen-mt uppercase text-black text-right'>Pro<span className='text-white'>yectos</span></h2>
+            <p className='text-xs font-inter italic text-black mt-2 leading-[200%] hidden'>Aquí puedes ver algunos de los proyectos en los que he trabajado.</p>
+            <div className='flex flex-col gap-4 mt-6'>
+              {proyectos.map((proyecto) => (
+                <a key={proyecto.id} className="text-[13px] text-white border-b border-black/30 border-dashed pb-2 text-right" href={proyecto.url} target="_blank" rel="noopener noreferrer">{proyecto.nombre}</a>
+              ))}
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
